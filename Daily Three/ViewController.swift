689558b090b81ecb0bb0a434a0d07fafd6687e1c
mollietaylor <mollie.taylor@gmail.com>
2015-02-08 20:29:46 -0500

    Initial Commit

---
+++ Daily Three/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Daily Three
+//
+//  Created by Mollie on 2/8/15.
+//  Copyright (c) 2015 Proximity Viz LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
